46f43ade0896487222ef4a8328de0eefd338712d
Jonathon <jonathon.biala@gmail.com>
2026-01-04 18:30:00 -0800

    Day 17: Add state and conditionals to toggle title

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ title.textContent = "Welcome to My Learning Journal";
 
 const button = document.getElementById("change-title-btn");
 
+let isOriginalTitle = true;
+
 button.addEventListener("click", () => {
-    title.textContent = "Title Changed by JavaScript"
+    if (isOriginalTitle) {
+        title.textContent = "Title Changed by JavaScript";
+        isOriginalTitle = false;
+    } else {
+        title.textContent = "Welcome to my Learning Journal"
+        isOriginalTitle = true;
+    }
 });
